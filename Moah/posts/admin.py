a44c9b74478568886e0ab7ed8a8c51e948b075a7
yujin <ohyuj2001@naver.com>
2023-06-30 19:29:55 +0900

    checkbox 추가

--- Moah/posts/admin.py
+++ Moah/posts/admin.py
@@ -4,4 +4,4 @@ from .models import Post
 
 @admin.register(Post)
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ('todolist')
+    list_display = ('checkbox','todolist')
--- Moah/posts/models.py
+++ Moah/posts/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
+    checkbox = models.BooleanField(verbose_name="체크", default=False)
     todolist = models.TextField(verbose_name='내용')
