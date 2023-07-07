30cf934942bc595f4e0abf7308cc4cbf06e6ec00
yujin <ohyuj2001@naver.com>
2023-07-07 18:48:53 +0900

    갤러리

--- Moah/gallery/urls.py
+++ Moah/gallery/urls.py
@@ -5,14 +5,25 @@ from django.urls import path, include
 #from posts.views import index
 
 
-from gallery.views import Jan
+from gallery.views import Feb, Jan
+
 app_name = 'gallery'
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('accounts/', include('accounts.urls', namespace='accounts')),
 
-    path('', Jan.as_view(), name='jan'),
-    
+    path('jan/', Jan.as_view(), name='jan'),
+    path('feb/', Feb.as_view(), name='feb'),
+    path('mar/', Feb.as_view(), name='mar'),
+    path('apr/', Feb.as_view(), name='apr'),
+    path('may/', Feb.as_view(), name='may'),
+    path('jun/', Feb.as_view(), name='jun'),
+    path('jul/', Feb.as_view(), name='jul'),
+    path('aug/', Feb.as_view(), name='aug'),
+    path('sep/', Feb.as_view(), name='sep'),
+    path('oct/', Feb.as_view(), name='oct'),
+    path('nov/', Feb.as_view(), name='nov'),
+    path('dec/', Feb.as_view(), name='dec'),
     
     #path('', index, name='index'),
     #path('posts/', include('posts.urls', namespace='post')),
--- Moah/gallery/views.py
+++ Moah/gallery/views.py
@@ -1,20 +1,21 @@
 from django.views.generic import TemplateView
 
-class Main(TemplateView):
-    template_name ='main.html'
+# class Main(TemplateView):
+#     template_name ='main.html'
     
-class Todo(TemplateView):
-    template_name ='todo/moahtodo.html'
+# class Todo(TemplateView):
+#     template_name ='todo/moahtodo.html'
     
-class Diary(TemplateView):
-    template_name ='Diary/diary.html'
+# class Diary(TemplateView):
+#     template_name ='Diary/diary.html'
     
-class Event(TemplateView):
-    template_name ='event.html'
+# class Event(TemplateView):
+#     template_name ='event.html'
     
-class Gallery(TemplateView):
-    template_name ='Gallery/Gallery-Jul.html'
+# class Gallery(TemplateView):
+#     template_name ='Gallery/Gallery-Jul.html'
 
+##########################################
 class Jan(TemplateView):
     template_name ='Gallery/Gallery-Jan.html'
 
