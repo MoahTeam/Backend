165081461ad6413d44a5b015c998fa6354f25442
ljy6712 <a01045386712@gmail.com>
2023-07-10 00:32:38 +0900

    account 유효성 검사 추가

--- Moah/accounts/views.py
+++ Moah/accounts/views.py
@@ -5,19 +5,25 @@ from django.contrib import auth
 from django.contrib.auth import login, logout
 from users.models import User
 from django.shortcuts import render, redirect
+import re
 
 #from sqlalchemy.exc import IntegrityError
 
 #유효성 검사, 비번찾기
 
 def signup_view(request):
+    #aaaa@aaaa.aaa
+    p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
     if request.method == 'POST':
         username = "hey"
         email = request.POST.get('email')
         password = request.POST.get('password')
         passwordCk = request.POST.get('passwordCk')
         if email == '' or password == "":
-            return render(request, 'Account/join.html')
+            return render(request, 'Account/join.html', {'error' : 'email'})
+        else:
+            if p.match(email) == False:
+                return render(request, 'Account/join.html', {'error' : 'email'})
         if password == passwordCk:
             try: user = User.objects.create_user(
                 username = username,
@@ -29,7 +35,8 @@ def signup_view(request):
             
             auth.login(request, user)
             return redirect('/')
-        return render(request, 'Account/join.html')
+        else:
+            return render(request, 'Account/join.html', {'error' : 'password'})
     else:
         return render(request, 'Account/join.html')
 
@@ -41,11 +48,11 @@ def login_view(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         if email == "":
-            pass
+            return render(request, 'Account/login.html', {'error' : 'email'})
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
-            return render(request, 'Account/login.html')
+            return render(request, 'Account/login.html', {'error': 'error'})
         except User.ValueError:
             return render(request, 'Account/login.html')
 
@@ -53,7 +60,7 @@ def login_view(request):
             auth.login(request, user)
             return redirect('main')
         else:
-            return render(request, 'Account/login.html')
+            return render(request, 'Account/login.html', {'error' : 'password'})
         
 def find(request):
     #make_random_password(length=10, allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
