83dcb00dff1fbb1350ef6fce996d5a0592942318
Jun279 <33.beautifulboy@gmail.com>
2023-07-13 22:37:09 +0900

    Revert "실험 6"
    This reverts commit fa03ceb378c2991c3ed3daf0a1cdb0c11b7ac605.

--- Moah/static/js/moahtodo.js
+++ Moah/static/js/moahtodo.js
@@ -45,9 +45,9 @@ plusButton.addEventListener('click',function() {
         var listItem = deleteButton.closest(".toDoList");
         listItem.remove();
     });
-    // var clonedForm = clone.querySelector("form");
-    // clonedForm.style.display = "inherit";
-    // clonedForm.style.alignItems = "center";
+    var clonedForm = clone.querySelector("form");
+    clonedForm.style.display = "inherit";
+    clonedForm.style.alignItems = "center";
     // clonedForm.style.margin = "auto";
     document.body.appendChild(clone);
 });
